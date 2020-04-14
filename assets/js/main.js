dc334636d2915119e84658f465676214ddd2e086
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-04-14 17:54:09 +0200

    First toggle ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
   console.log('il documento è pronto');
+
+
+  // reference
+  var dropDown = $('.primary-list .dropdown-li');
+  var dropDwnLinks = dropDown.children('a');
+  var dropDwnMenus = dropDown.children('.dropdown-menu');
+
+  dropDwnLinks.click(function(){
+    var actualMenu = $(this).next('.dropdown-menu');
+
+    dropDwnMenus.not(actualMenu).hide();
+    
+    actualMenu.toggle();
+  });
+
 });//ready
